58927059dcf83e95867a18936b32bbae9e7fbbea
BobLouis <bob900316@gmail.com>
2022-04-08 15:54:49 +0800

    hw3 up:

--- HW3/LZ77_Encoder.v
+++ HW3/LZ77_Encoder.v
@@ -7,9 +7,9 @@ input 		[7:0] 	chardata;
 output  			valid;
 output  			encode;
 output  			finish;
-output 		[3:0] 	offset;
-output 		[2:0] 	match_len;
-output 	 	[7:0] 	char_nxt;
+output 	reg	[3:0] 	offset;
+output 	reg	[2:0] 	match_len;
+output 	reg [7:0] 	char_nxt;
 
 
 /*
@@ -23,11 +23,12 @@ parameter READ = 3'b001;
 parameter CAL = 3'b010;
 parameter OUT = 3'b011;
 
-reg [7:0]str[0:2048];
+reg [7:0]str[0:2048]; 
 reg [12:0]cnt;
 reg [7:0] buffer[0:16];
-reg [12:0]str_idx;
-reg [3:0] window_dix;
+reg [12:0]str_ptr;
+reg [3:0]match_len_tmp;
+reg [7:0]match;
 wire [7:0] window[0:8];
 integer i;
 
@@ -52,11 +53,11 @@ always@(*)begin
                 else next_state = READ;   
             end
             CAL:begin
-                if() next_state = OUT;
+                if(cnt == 9) next_state = OUT;
                 else next_state = CAL;
             end 
             OUT:
-                if() next_state = FINISH;
+                if(str_ptr == 2047) next_state = FINISH;
                 else next_state = CAL;
             FINISH:begin
                 next_state = FINISH;
@@ -73,56 +74,112 @@ end
 always@(posedge clk or posedge reset)begin
     if(reset)
         cnt <= 0;
-    else if(state == READ)
-        cnt <= cnt + 13'd1;
-    else 
+    else if(next_state == READ && cnt == 2048)
         cnt <= 0;
+    else if(next_state == CAL && cnt == 9)
+        cnt <= 0;
+    else if(next_state == OUT)
+        cnt <= 0;
+    else 
+        cnt <= cnt + 1;
 end
 
-//DATA INPUT
+//DATA INPUTs
 always@(posedge clk or posedge reset)begin
     if(reset)begin
         str [2048]<= 8'h24;
     end
-    else if(next_state ==READ)begin
+    else if(next_state == READ)begin
         str[cnt] <= chardata;
     end
 end
 
 
-//str_index
+//str_ptr
 always @(posedge clk or posedge reset) begin
 	if(reset)begin
-		str_idx <= 0;
+		str_ptr <= 8;
 	end
 	else if(next_state == CAL)begin
 		if(cnt == 0)begin
-			str_idx <= 8;
+			str_ptr <= 8;
 		end
 	end
 end
+
+
+//window_idx
+always @(posedge clk or posedge reset) begin
+    if(reset)begin
+        window_idx <= 0;
+    else if(next_state == CAL && window_idx < 8)begin
+        window_idx <= window_idx + 1;
+    end
+    else if(next_state == CAL && window_idx == 8)begin
+        window_idx <= 0;
+    end
+end
+
 
 assign window = buffer[16-window_idx:8-window_idx];
 
 //CAL
 always @(posedge clk or posedge reset) begin
 	if(reset)begin
-		str_idx <= 0;
-		for(i = 0; i < 17 ; i = i +1)begin
-			buffer[i] <=0 ;
+        //search buffer
+		for(i = 8; i < 17 ; i = i +1)begin
+			buffer[i] <= 0 ;
 		end
 	end
-	if(state == CAL)begin
+    else if(state == READ && cnt ==2046)
+    begin
+        //look ahead buufer
+        for(i = 0; i < 8; i = i + 1)
+            buffer[7-i] <= str[i];
+    end
+	else if(state == CAL)begin
 		if(cnt == 0)begin
-			//init
-			for(i = 0; i <8; i = i + 1)
-				buffer[7-i] <= str[i];
+            match_len <= 0;
+            offset    <= 0;
+            char_nxt  <= buffer[7];
 		end
 		else begin
-			win
+			if(match_len_tmp > match_len)begin
+                match_len <= match_len_tmp;
+                offset <= window_idx;
+                char_nxt <= buffer[8-window_idx]
+            end
 		end
 	end
 end
+
+//compare window with look ahead
+always @(*) begin
+    match[0] = window[8] == buffer[7]; 
+    match[1] = window[7] == buffer[6]; 
+    match[2] = window[6] == buffer[5]; 
+    match[3] = window[5] == buffer[4]; 
+    match[4] = window[4] == buffer[3]; 
+    match[5] = window[3] == buffer[2]; 
+    match[6] = window[2] == buffer[1]; 
+    match[7] = window[1] == buffer[0]; 
+end
+
+always @(*) begin
+    casex(match)
+        8'b11111111: match_len_tmp = 8;
+        8'b11111110: match_len_tmp = 7;
+        8'b1111110x: match_len_tmp = 6;
+        8'b111110xx: match_len_tmp = 5;
+        8'b11110xxx: match_len_tmp = 4;
+        8'b1110xxxx: match_len_tmp = 3;
+        8'b110xxxxx: match_len_tmp = 2;
+        8'b10xxxxxx: match_len_tmp = 1;
+        8'b0xxxxxxx: match_len_tmp = 0;
+    endcase
+end
+
+
 
 //OUTPUT
 always@(*)begin
