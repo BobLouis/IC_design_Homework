e7976f0464b80a7159cd8141a5fc384f9fd8e140
BobLouis <bob900316@gmail.com>
2022-04-13 23:12:58 +0800

    decoder complete

--- HW3/LZ77_Decoder.v
+++ HW3/LZ77_Decoder.v
@@ -7,7 +7,7 @@ input 		[2:0] 	code_len;
 input 		[7:0] 	chardata;
 output  			encode;
 output  			finish;
-output 	 	[7:0] 	char_nxt;
+output 	 reg   [7:0]char_nxt;
 
 
 /*
@@ -17,14 +17,30 @@ assign encode = 1'b0;
 assign finish = (char_nxt == 8'h24)?1:0;
 reg [7:0]buff[0:8];
 reg [3:0]cnt;
-wire b1 = (1 <= code_len)?(code_pos-len+1):(0);
-wire b2 = (2 <= code_len)?(code_pos-len+1):(1-len);
-wire b3 = (3 <= code_len)?(code_pos-len+1):(2-len);
-wire b4 = (4 <= code_len)?(code_pos-len+1):(3-len);
-wire b5 = (5 <= code_len)?(code_pos-len+1):(4-len);
-wire b6 = (6 <= code_len)?(code_pos-len+1):(5-len);
-wire b7 = (7 <= code_len)?(code_pos-len+1):(6-len);
-wire b8 = (8 <= code_len)?(code_pos-len+1):(7-len);
+reg [20:0]k;
+integer i;
+// wire [3:0]b1 = (1 <= code_len)?((code_pos+1)>=code_len?code_pos-code_len+1:~(code_pos-code_len+1)+1):0;
+// wire [3:0]b2 = (2 <= code_len)?((code_pos+2)>=code_len?code_pos-code_len+2:~(code_pos-code_len+2)+1):(1-code_len);
+// wire [3:0]b3 = (3 <= code_len)?((code_pos+3)>=code_len?code_pos-code_len+3:~(code_pos-code_len+3)+1):(2-code_len);
+// wire [3:0]b4 = (4 <= code_len)?((code_pos+4)>=code_len?code_pos-code_len+4:~(code_pos-code_len+4)+1):(3-code_len);
+// wire [3:0]b5 = (5 <= code_len)?((code_pos+5)>=code_len?code_pos-code_len+5:~(code_pos-code_len+5)+1):(4-code_len);
+// wire [3:0]b6 = (6 <= code_len)?((code_pos+6)>=code_len?code_pos-code_len+6:~(code_pos-code_len+6)+1):(5-code_len);
+// wire [3:0]b7 = (7 <= code_len)?((code_pos+7)>=code_len?code_pos-code_len+7:~(code_pos-code_len+7)+1):(6-code_len);
+// wire [3:0]b8 = (8 <= code_len)?((code_pos+8)>=code_len?code_pos-code_len+8:~(code_pos-code_len+8)+1):(7-code_len);
+wire [3:0]b1 = (1 <= code_len)?code_pos - ((code_len - 1) % (code_pos+1)):0;
+wire [3:0]b2 = (2 <= code_len)?code_pos - ((code_len - 2) % (code_pos+1)):(1-code_len);
+wire [3:0]b3 = (3 <= code_len)?code_pos - ((code_len - 3) % (code_pos+1)):(2-code_len);
+wire [3:0]b4 = (4 <= code_len)?code_pos - ((code_len - 4) % (code_pos+1)):(3-code_len);
+wire [3:0]b5 = (5 <= code_len)?code_pos - ((code_len - 5) % (code_pos+1)):(4-code_len);
+wire [3:0]b6 = (6 <= code_len)?code_pos - ((code_len - 6) % (code_pos+1)):(5-code_len);
+wire [3:0]b7 = (7 <= code_len)?code_pos - ((code_len - 7) % (code_pos+1)):(6-code_len);
+wire [3:0]b8 = (8 <= code_len)?code_pos - ((code_len - 8) % (code_pos+1)):(7-code_len);
+always @(posedge clk or posedge reset) begin
+	if(reset)
+		k <= 4;
+	else 
+		k <= k+1;
+end
 //buffer
 always @(posedge clk or posedge reset) begin
 	if(reset)begin
@@ -32,7 +48,7 @@ always @(posedge clk or posedge reset) begin
 			buff[i] <= 0;
 		end
 	end
-	else begin
+	else if(cnt == 0)begin
 		buff[0] <= chardata;
 		buff[1] <= buff[b1];
 		buff[2] <= buff[b2];
@@ -42,6 +58,7 @@ always @(posedge clk or posedge reset) begin
 		buff[6] <= buff[b6];
 		buff[7] <= buff[b7];
 		buff[8] <= buff[b8];
+	end
 end
 //cnt
 always @(posedge clk or posedge reset) begin
@@ -56,7 +73,7 @@ always @(posedge clk or posedge reset) begin
 	end
 end
 
-//output
+// output
 always @(posedge clk or posedge reset) begin
 	if(reset)begin
 		char_nxt <= 0;
@@ -66,11 +83,29 @@ always @(posedge clk or posedge reset) begin
 			char_nxt <= chardata;
 		end
 		else begin //len > 0
-			char_nxt <= buff[code_len - cnt];
+			if(cnt == 0)
+				char_nxt <= buff[code_pos];
+			else
+				char_nxt <= buff[code_len - cnt];
 		end
 	end
 end
 
+// always @(*) begin
+// 	if(reset)begin
+// 		char_nxt = 0;
+// 	end
+// 	else if(code_len == 0)begin
+// 		char_nxt = chardata;
+// 	end
+// 	else begin
+// 		char_nxt = buff[code_len - cnt];
+// 	end
+// end
+
+assign finish = (char_nxt == 8'h24)?1:0;
+// assign finish = 0;
+assign encode = 0;
 
 
 endmodule
