658333af14b0c83c8685907fd3001b9a2129aba8
BobLouis <bob900316@gmail.com>
2022-05-24 04:53:40 +0800

    Q2_2complete

--- FINAL/Q2_2/Q2_Decoder.v
+++ FINAL/Q2_2/Q2_Decoder.v
@@ -12,7 +12,42 @@ output 		[7:0] 	char_nxt;
 
 
 /* write your code here ! */
+reg [7:0]buff[0:9];
+reg [2:0]cnt;
+//buffer
 
+
+always @(posedge clk or posedge reset) begin
+	if(reset)begin
+		buff[0] <= 0;
+	end
+	else begin
+		buff[0] <= (cnt == code_len)?chardata:buff[code_pos];
+		buff[1] <= buff[0];
+		buff[2] <= buff[1];
+		buff[3] <= buff[2];
+		buff[4] <= buff[3];
+		buff[5] <= buff[4];
+		buff[6] <= buff[5];
+		buff[7] <= buff[6];
+		buff[8] <= buff[7];
+		buff[9] <= buff[8];
+	end
+end
+always @(posedge clk or posedge reset) begin
+	if(reset)
+		cnt <= 0;
+	else begin
+		if(cnt == code_len)begin
+			cnt <= 0;
+		end
+		else 
+			cnt <= cnt + 1;
+	end
+end
+assign char_nxt = buff[0];
+assign finish = (char_nxt == 8'h24)?1:0;
+assign encode =  0;
 
 endmodule
 
